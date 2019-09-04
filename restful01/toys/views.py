d8140b3dbed6654552c572aaf17aadf66106e7a3
Pramochete <orientedresearch@gmail.com>
2019-09-04 12:14:59 +0545

    urls created

--- restful01/toys/views.py
+++ restful01/toys/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework import status
 from django.views.decorators.csrf import csrf_exempt
-from rest_framework.decorators.csrf import csrf_exempt
 from rest_framework.renderers import JSONRenderer
 from rest_framework.parsers import JSONParser
-from rest_framework import status
 from toys.models import Toy
 from toys.serializers import ToySerializer
 
