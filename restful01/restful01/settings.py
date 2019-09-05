93bd7dc7f42ca07604be493dc67c918759b03f20
Pramochete <orientedresearch@gmail.com>
2019-09-05 15:54:29 +0545

    updated postgresql settings

--- restful01/restful01/settings.py
+++ restful01/restful01/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'restful01.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'drones',
+        'USER': 'rest',
+        'PASSWORD': 'qbasic919',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
